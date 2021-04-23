9fbd03b449d391810e90768aa9ed91192496929d
James Duarte <GarnetSunset@users.noreply.github.com>
2021-04-23 14:04:00 -0400

    wow lots of changes

--- autoBuild.bat
+++ autoBuild.bat
@@ -1,4 +1,5 @@
 ParTool add -c 1 ..\Y6DEMO\ui.par ui/ ..\Y6DEMO\Image0\data\ui.par
-xcopy data\ ..\Y6DEMO\Image0\ /S /Y
-xcopy sce_sys\ ..\Y6DEMO\Image0\ /S /Y
+xcopy data\ ..\Y6DEMO\Image0\data\ /S /Y
+xcopy sce_sys\ ..\Y6DEMO\Image0\sce_sys\ /S /Y
+xcopy ps4\ ..\Y6DEMO\Image0\ps4\ /S /Y
 ..\Y6DEMO\gengp4.exe ..\Y6DEMO\Image0
