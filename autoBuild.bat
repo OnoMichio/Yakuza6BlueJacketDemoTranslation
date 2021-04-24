2137ca24e07946ef890f7f4df403a1625f57b917
James Duarte <GarnetSunset@users.noreply.github.com>
2021-04-24 16:33:21 -0400

    pushes build to USB

--- autoBuild.bat
+++ autoBuild.bat
@@ -6,3 +6,4 @@ xcopy data\ %y6demolocation%\Image0\data\ /S /Y
 xcopy sce_sys\ %y6demolocation%\Image0\sce_sys\ /S /Y
 xcopy ps4\ %y6demolocation%\Image0\ps4\ /S /Y
 %y6demolocation%\gengp4.exe %y6demolocation%\Image0
+%y6demolocation%\newer\orbis-pub-cmd img_create %y6demolocation%\Image0.gp4 H:\JP0177-CUSA04008_00-RYU6DEMO00000000-A0100-V0100.pkg
