6f2a6b77b436f81b8fb66f45ee5ddd725f2df6f6
James Duarte <GarnetSunset@users.noreply.github.com>
2021-04-24 16:34:15 -0400

    add another variable

--- autoBuild.bat
+++ autoBuild.bat
@@ -1,4 +1,5 @@
 set y6demolocation=..\Y6DEMO
+set outputLoc=H:\JP0177-CUSA04008_00-RYU6DEMO00000000-A0100-V0100.pkg
 ParTool add -c 1 %y6demolocation%\ui.par ui/ %y6demolocation%\Image0\data\ui.par
 ParTool add -c 1 %y6demolocation%\Image0\data\auth\t01_010_01.par.bak data/auth/t01_010_01.par.unpack/ %y6demolocation%\Image0\data\auth\t01_010_01.par
 ParTool add -c 1 %y6demolocation%\Image0\data\auth\t01_090_01.par.bak data/auth/t01_090_01.par.unpack/ %y6demolocation%\Image0\data\auth\t01_090_01.par
@@ -6,4 +7,4 @@ xcopy data\ %y6demolocation%\Image0\data\ /S /Y
 xcopy sce_sys\ %y6demolocation%\Image0\sce_sys\ /S /Y
 xcopy ps4\ %y6demolocation%\Image0\ps4\ /S /Y
 %y6demolocation%\gengp4.exe %y6demolocation%\Image0
-%y6demolocation%\newer\orbis-pub-cmd img_create %y6demolocation%\Image0.gp4 H:\JP0177-CUSA04008_00-RYU6DEMO00000000-A0100-V0100.pkg
+%y6demolocation%\newer\orbis-pub-cmd img_create %y6demolocation%\Image0.gp4 %outputLoc%
