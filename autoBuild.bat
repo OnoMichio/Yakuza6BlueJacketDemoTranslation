fa9d38cc51646e96cdc7bf5f8d9fd1c53b099577
James Duarte <GarnetSunset@users.noreply.github.com>
2021-04-24 16:07:15 -0400

    fuckin awesome

--- autoBuild.bat
+++ autoBuild.bat
@@ -1,7 +1,8 @@
-ParTool add -c 1 ..\Y6DEMO\ui.par ui/ ..\Y6DEMO\Image0\data\ui.par
-ParTool add -c 1 ..\Y6DEMO\Image0\data\auth\t01_010_01.par.bak data/auth/t01_010_01.par.unpack/ ..\Y6DEMO\Image0\data\auth\t01_010_01.par
-ParTool add -c 1 ..\Y6DEMO\Image0\data\auth\t01_090_01.par.bak data/auth/t01_090_01.par.unpack/ ..\Y6DEMO\Image0\data\auth\t01_090_01.par
-xcopy data\ ..\Y6DEMO\Image0\data\ /S /Y
-xcopy sce_sys\ ..\Y6DEMO\Image0\sce_sys\ /S /Y
-xcopy ps4\ ..\Y6DEMO\Image0\ps4\ /S /Y
-..\Y6DEMO\gengp4.exe ..\Y6DEMO\Image0
+set y6demolocation=..\Y6DEMO
+ParTool add -c 1 %y6demolocation%\ui.par ui/ %y6demolocation%\Image0\data\ui.par
+ParTool add -c 1 %y6demolocation%\Image0\data\auth\t01_010_01.par.bak data/auth/t01_010_01.par.unpack/ %y6demolocation%\Image0\data\auth\t01_010_01.par
+ParTool add -c 1 %y6demolocation%\Image0\data\auth\t01_090_01.par.bak data/auth/t01_090_01.par.unpack/ %y6demolocation%\Image0\data\auth\t01_090_01.par
+xcopy data\ %y6demolocation%\Image0\data\ /S /Y
+xcopy sce_sys\ %y6demolocation%\Image0\sce_sys\ /S /Y
+xcopy ps4\ %y6demolocation%\Image0\ps4\ /S /Y
+%y6demolocation%\gengp4.exe %y6demolocation%\Image0
