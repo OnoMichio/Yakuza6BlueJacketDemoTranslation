03d79611b7385d14a401242b2bc651a23a4fee8b
barnfind <79670537+barnfind@users.noreply.github.com>
2021-04-25 23:04:41 +0100

    Update autoBuild.bat

--- autoBuild.bat
+++ autoBuild.bat
@@ -3,6 +3,7 @@ set outputLoc=H:\JP0177-CUSA04008_00-RYU6DEMO00000000-A0100-V0100.pkg
 echo & echo.| ParTool add -c 1 %y6demolocation%\ui.par ui/ %y6demolocation%\Image0\data\ui.par
 echo & echo.| ParTool add -c 1 %y6demolocation%\Image0\data\auth\t01_010_01.par.bak data/auth/t01_010_01.par.unpack/ %y6demolocation%\Image0\data\auth\t01_010_01.par
 echo & echo.| ParTool add -c 1 %y6demolocation%\Image0\data\auth\t01_090_01.par.bak data/auth/t01_090_01.par.unpack/ %y6demolocation%\Image0\data\auth\t01_090_01.par
+echo & echo.| ParTool add -c 1 %y6demolocation%\Image0\data\motion data/motion.par.unpack %y6demolocation%\Image0\data\motion
 xcopy data\ %y6demolocation%\Image0\data\ /S /Y
 xcopy sce_sys\ %y6demolocation%\Image0\sce_sys\ /S /Y
 xcopy ps4\ %y6demolocation%\Image0\ps4\ /S /Y
