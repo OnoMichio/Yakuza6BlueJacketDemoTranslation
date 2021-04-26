f1b0f56c528ac8a9824284f473e9bb4a105ca00b
OnoMichio <32009437+OnoMichio@users.noreply.github.com>
2021-04-26 16:52:26 +0100

    Update autoBuild.bat

--- autoBuild.bat
+++ autoBuild.bat
@@ -2,6 +2,7 @@ set y6demolocation=..\Y6DEMO
 set outputLoc=H:\JP0177-CUSA04008_00-RYU6DEMO00000000-A0100-V0100.pkg
 echo & echo.| ParTool add -c 1 %y6demolocation%\ui.par ui/ %y6demolocation%\Image0\data\ui.par
 echo & echo.| ParTool add -c 1 %y6demolocation%\Image0\data\auth\t01_010_01.par.bak data/auth/t01_010_01.par.unpack/ %y6demolocation%\Image0\data\auth\t01_010_01.par
+echo & echo.| ParTool add -c 1 %y6demolocation%\Image0\data\auth\t01_060_02.par.bak data/auth/t01_060_02.par.unpack/ %y6demolocation%\Image0\data\auth\t01_060_02.par
 echo & echo.| ParTool add -c 1 %y6demolocation%\Image0\data\auth\t01_090_01.par.bak data/auth/t01_090_01.par.unpack/ %y6demolocation%\Image0\data\auth\t01_090_01.par
 echo & echo.| ParTool add -c 1 %y6demolocation%\Image0\data\motion.par.bak data/motion %y6demolocation%\Image0\data\motion.par
 xcopy data\ %y6demolocation%\Image0\data\ /S /Y
